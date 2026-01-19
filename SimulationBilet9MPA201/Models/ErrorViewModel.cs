727910614e8e8150366c3e3aef9a3e9b9bd9284e
ElchinAliyevCode <elchinfa-mpa201@code.edu.az>
2026-01-19 15:06:58 +0400

    Add project files.

---
+++ SimulationBilet9MPA201/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimulationBilet9MPA201.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
